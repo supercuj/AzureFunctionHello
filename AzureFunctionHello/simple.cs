c833c70bd5fe1a2d7c0372cb812b95c734b870fc
Joseph Ruedlinger <supercuj@hotmail.com>
2021-02-02 19:02:03 -0800

    Add Please Leave

--- AzureFunctionHello/simple.cs
+++ AzureFunctionHello/simple.cs
@@ -72,6 +72,26 @@ namespace AzureFunctionHello
             return new OkObjectResult(responseMessage);
         }
 
+        [FunctionName("PleaseLeave")]
+        public static async Task<IActionResult> Run4(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Please Leave Now, {name}. The Time is: {DateTime.Now.ToString()}";
+
+            return new OkObjectResult(responseMessage);
+        }
+
 
 
 
