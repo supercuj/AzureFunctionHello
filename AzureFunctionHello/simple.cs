667cb2c97f991ef19754bccc3a17d1dadcd07daa
Joseph Ruedlinger <supercuj@hotmail.com>
2021-02-02 18:53:15 -0800

    Add WelcomeBack entrypoint

--- AzureFunctionHello/simple.cs
+++ AzureFunctionHello/simple.cs
@@ -52,6 +52,27 @@ namespace AzureFunctionHello
             return new OkObjectResult(responseMessage);
         }
 
+        [FunctionName("WelcomeBack")]
+        public static async Task<IActionResult> Run3(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Welcome Back, {name}. The Time is: {DateTime.Now.ToString()}";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+
 
 
     }
